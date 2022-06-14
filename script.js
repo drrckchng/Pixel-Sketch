c1a9ff21edc4a81e72feb50186e77998ab2ba31e
drrckchng <derrickchungx39@gmail.com>
2022-06-14 09:55:37 +0900

    Add function to create grid with variable dimension

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function createGrid(size) {
-    for(let i = 0; i < size; i++) {
+    for(let i = 0; i < size ** 2; i++) {
         const oneGrid = document.createElement('div');
         oneGrid.classList.add('grid');
         container.appendChild(oneGrid);
     }
+    container.style.gridTemplate = `repeat(${size}, 1fr)/repeat(${size}, 1fr)`;
 }
 
 const container = document.getElementById('container');
-createGrid(16);
+createGrid(20);
